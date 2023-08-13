5a125d4855a808028d13334774f3945f5856dac4
Xposeded <26787211+Xposeded@users.noreply.github.com>
2023-08-13 14:32:57 +0800

    Now the light will go double the distance than before.

--- src/main/java/aitirava/furtherlights/server/mixins/world/chunk/light/ChunkBlockLightProviderMixin.java
+++ src/main/java/aitirava/furtherlights/server/mixins/world/chunk/light/ChunkBlockLightProviderMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.world.chunk.light.ChunkBlockLightProvider;
 import net.minecraft.world.chunk.light.ChunkLightProvider;
 import net.minecraft.world.chunk.light.LightStorage;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,6 +18,9 @@ import static net.minecraft.world.RedstoneView.DIRECTIONS;
 
 @Mixin(ChunkBlockLightProvider.class)
 public class ChunkBlockLightProviderMixin {
+
+    @Unique
+    private static final int AMPLIFIER = 2;
 
     @Inject(
             method = "method_51529",
@@ -33,9 +37,11 @@ public class ChunkBlockLightProviderMixin {
         if (!((LightStorageInvoker) lightStorage).invokerHasSection(sourceSectionPos)) {
             return;
         }
+
         int sourceLightLevel= ((LightStorageInvoker) lightStorage).invokerGet(sourceBlockPos);
         BlockState blockState = ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos.set(sourceBlockPos));
         int lightEmitterLuminance = ((ChunkBlockLightProviderInvoker) this).invokerGetLightSourceLuminance(sourceBlockPos, blockState);
+
         if (lightEmitterLuminance < sourceLightLevel ) {
             ((LightStorageInvoker) lightStorage).invokerSet(sourceBlockPos, 0);
             ((ChunkLightProviderInvoker) this).invokerQueueForLightDarkening(
@@ -65,44 +71,52 @@ public class ChunkBlockLightProviderMixin {
                 Blocks.AIR.getDefaultState()
                 :
                 ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos.set(sourceBlockPos));
-//        int decay = 0;
 
-//        for (Direction direction : DIRECTIONS) {
-//            long blockPos = BlockPos.offset(sourceBlockPos, direction);
-//            mutablePos.set(blockPos);
-//            BlockState targetBlockState = ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos);
-//            int targetOpacity = ((ChunkLightProviderInvoker) this).invokerGetOpacity(targetBlockState, mutablePos);
-//            if (targetOpacity > 1) decay += 2;
-//        }
-//        if (decay > 2) decay = 2;
-//        sourceLightLevel -= decay ; // Decay light if source is occluded
+        int Opacity = 0;
+
+        int decay = 0;
+        for (Direction direction : DIRECTIONS) {
+            long blockPos = BlockPos.offset(sourceBlockPos, direction);
+            mutablePos.set(blockPos);
+            BlockState targetBlockState = ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos);
+            int targetOpacity = ((ChunkLightProviderInvoker) this).invokerGetOpacity(targetBlockState, mutablePos);
+            if (targetOpacity > 1) decay += 2;
+
+            Opacity |= (targetOpacity << (direction.getId() * 4));
+        }
+//        sourceLightLevel -= decay ; // Decay light if source is occluded, haven't implemented.
 
         for (Direction direction : DIRECTIONS) {
             long targetBlockPos = BlockPos.offset(sourceBlockPos, direction);
+
+            if (!((LightStorageInvoker) lightStorage).
+                    invokerHasSection(ChunkSectionPos.fromBlockPos(targetBlockPos))
+            ) continue;
+
             int targetLightLevel_Old = ((LightStorageInvoker) lightStorage).invokerGet(targetBlockPos);
 
             // Can only propagate to darker, existing areas.
             if (
                     !ChunkLightProvider.class_8531.isDirectionBitSet(packedData, direction)
-                            || !((LightStorageInvoker) lightStorage).invokerHasSection(ChunkSectionPos.fromBlockPos(targetBlockPos))
-                            || (sourceLightLevel - 1) <= (targetLightLevel_Old)
+                            ||  (sourceLightLevel - 1) <= (targetLightLevel_Old)
             ) continue;
+
             mutablePos.set(targetBlockPos);
             BlockState targetBlockState = ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos);
-            int targetOpacity = ((ChunkLightProviderInvoker) this).invokerGetOpacity(targetBlockState, mutablePos);
+            int targetOpacity = 0xF & (Opacity >> (direction.getId() * 4));
             int targetLightLevel_New =
                     sourceLightLevel - targetOpacity;
 
             // Propagated light must be brighter.
             if (targetLightLevel_New <= targetLightLevel_Old) continue;
 
-//            if (targetLightLevel_New < 10) {
-//                int sum =
-//                        BlockPos.unpackLongX(targetBlockPos)
-//                                + BlockPos.unpackLongY(targetBlockPos)
-//                                + BlockPos.unpackLongZ(targetBlockPos);
-//                if (sum % 2 == 0 && targetOpacity == 1) targetLightLevel_New = sourceLightLevel;
-//            }
+            if (targetOpacity == 1) {
+                int sum =
+                        BlockPos.unpackLongX(targetBlockPos)
+                                + BlockPos.unpackLongY(targetBlockPos)
+                                + BlockPos.unpackLongZ(targetBlockPos);
+                if (sum % AMPLIFIER == 0 ) targetLightLevel_New = sourceLightLevel;
+            }
 
             if (((ChunkLightProviderInvoker) this).invokerShapesCoverFullCube(
                     sourceBlockPos, sourceBlockState, targetBlockPos, targetBlockState, direction)) continue;
@@ -135,28 +149,41 @@ public class ChunkBlockLightProviderMixin {
 
         int sourceLightLevel = ChunkLightProvider.class_8531.getLightLevel(packedData);
         for (Direction direction : DIRECTIONS) {
-            int targetLightLevel_Old;
             long targetBlockPos = BlockPos.offset(sourceBlockPos, direction);
-            if (
-                    !ChunkLightProvider.class_8531.isDirectionBitSet(packedData, direction)
-                            || !((LightStorageInvoker) lightStorage).invokerHasSection(
-                                    ChunkSectionPos.fromBlockPos(targetBlockPos))
-                            || (targetLightLevel_Old = ((LightStorageInvoker) lightStorage).invokerGet(targetBlockPos)) == 0
+
+            if( !((LightStorageInvoker) lightStorage).
+                    invokerHasSection(ChunkSectionPos.fromBlockPos(targetBlockPos))
             ) continue;
 
-            if (targetLightLevel_Old <= sourceLightLevel - 1) {
+            int targetLightLevel_Old = ((LightStorageInvoker) lightStorage).invokerGet(targetBlockPos);
+
+            if (
+                    !ChunkLightProvider.class_8531.isDirectionBitSet(packedData, direction)
+                            || targetLightLevel_Old  == 0
+            ) continue;
+
+            boolean bypass = false;
+            if (targetLightLevel_Old == sourceLightLevel) {
+                int sum =
+                        BlockPos.unpackLongX(targetBlockPos)
+                                + BlockPos.unpackLongY(targetBlockPos)
+                                + BlockPos.unpackLongZ(targetBlockPos);
+                if (sum % AMPLIFIER == 0 ) bypass = true;
+            }
+
+            if (targetLightLevel_Old <= sourceLightLevel - 1 || bypass) {
                 BlockState blockState = ((ChunkLightProviderInvoker) this).invokerGetStateForLighting(mutablePos.set(targetBlockPos));
-                int lightEmitterLuminance = ((ChunkBlockLightProviderInvoker) this).invokerGetLightSourceLuminance(targetBlockPos, blockState);
+                int TargetPosLightEmitterLuminance = ((ChunkBlockLightProviderInvoker) this).invokerGetLightSourceLuminance(targetBlockPos, blockState);
                 ((LightStorageInvoker) lightStorage).invokerSet(targetBlockPos, 0);
-                if (lightEmitterLuminance < targetLightLevel_Old) {
+                if (TargetPosLightEmitterLuminance < targetLightLevel_Old ) {
                     ((ChunkLightProviderInvoker) this).invokerQueueForLightDarkening(targetBlockPos, ChunkLightProvider.class_8531.packWithOneDirectionCleared(targetLightLevel_Old, direction.getOpposite()));
                 }
 
-                if (lightEmitterLuminance <= 0) continue;
+                if (TargetPosLightEmitterLuminance <= 0) continue;
 
                 ((ChunkLightProviderInvoker) this).invokerQueueForLightEnlightening(
                         targetBlockPos, ChunkLightProvider.class_8531.method_51573(
-                                lightEmitterLuminance, ChunkLightProviderInvoker.invokeIsTrivialForLighting(blockState)
+                                TargetPosLightEmitterLuminance, ChunkLightProviderInvoker.invokeIsTrivialForLighting(blockState)
                         )
                 );
                 continue;
